2291a17165afaa97f0e4268dea87a09a7ed2ab94
Mark Easterling <marke.easterling@gmail.com>
2016-09-20 13:44:49 -0500

    added deletion confirmation

--- client/app/controllers/DeleteListingCtrl.js
+++ client/app/controllers/DeleteListingCtrl.js
@@ -1,5 +1,13 @@
 app.controller("DeleteListingCtrl", function($http, $location, $routeParams, authFactory) {
   const deleteListing = this
 
-  $http.delete("http://localhost:8000/request/" + $routeParams.listingId +"/")
+  $http.get("http://localhost:8000/request/" + $routeParams.listingId +"/")
+    .then(result => deleteListing.listing = result.data)
+      .then(()=>{console.log(deleteListing.listing)})
+
+
+  deleteListing.delete = function() {
+    $http.delete("http://localhost:8000/request/" + $routeParams.listingId +"/")
+    .then(()=>{$location.path("/landing")})
+  }
 })
