90fa7701d08580a4104d63d2320063129c17a1ac
Mark Easterling <marke.easterling@gmail.com>
2016-09-20 10:18:09 -0500

    retreiving current user data, displaying listings

--- client/app/controllers/LandingCtrl.js
+++ client/app/controllers/LandingCtrl.js
@@ -1,7 +1,11 @@
-app.controller("LandingCtrl", function($http, $location, $routeParams) {
+app.controller("LandingCtrl", function($http, $location, $routeParams, authFactory) {
   const landing = this
   landing.title = "Landing"
+  landing.userId = authFactory.user.userId
 
-  
+  $http.get(`http://localhost:8000/user/${landing.userId}`)
+    .then(result => landing.user = result.data)
+      .then(() => console.log(landing.user))
 
-})
+
+    })
