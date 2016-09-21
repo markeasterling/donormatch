08bd9b8d0a095fa02e263f80998110e99e3290ca
Mark Easterling <marke.easterling@gmail.com>
2016-09-21 10:27:18 -0500

    filtering message results

--- client/app/controllers/ViewMessagesCtrl.js
+++ client/app/controllers/ViewMessagesCtrl.js
@@ -3,7 +3,7 @@ app.controller("ViewMessagesCtrl", function($http, $location, authFactory) {
   viewMessages.userPk = authFactory.user.userId
 
   $http.post("http://localhost:8000/get_messages",
-  {"user": 8}, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
+  {"user": viewMessages.userPk}, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
     .then(response => viewMessages.messages = response.data)
       .then(() => (console.log(viewMessages.messages)))
 })
