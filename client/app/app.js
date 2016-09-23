9cd0804ed573236fafd9faefac848467a4416947
Mark Easterling <marke.easterling@gmail.com>
2016-09-22 20:28:24 -0500

    landing page styling

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,5 @@
 var app = angular
-  .module("client", ["ngRoute"])
+  .module("client", ["ngRoute", "angular.filter"])
   .config(function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
--- client/app/controllers/LandingCtrl.js
+++ client/app/controllers/LandingCtrl.js
@@ -2,15 +2,16 @@ app.controller("LandingCtrl", function($http, $location, $routeParams, authFacto
   const landing = this
   landing.title = "Landing"
   landing.userPk = authFactory.user.userId
+  landing.name = authFactory.user.username
 
   $http.get(`http://localhost:8000/user/8`) //${landing.userId}
     .then(result => landing.user = result.data)
       .then(() => console.log(landing.user))
 
-      $http.post("http://localhost:8000/get_messages",
-      {"user": landing.userPk }, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
-        .then(response => landing.messages = response.data)
-          .then(() => (console.log(landing.messages)))
+    $http.post("http://localhost:8000/get_messages",
+    {"user": 8 }, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
+      .then(response => landing.messages = response.data)
+        .then(() => (console.log(landing.messages)))
 
   landing.newListing = function() {
     $location.path("/newlisting")
