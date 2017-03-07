31da6a101d8fd352a3371c8ab1dbf7f88c7710c0
Mark Easterling <marke.easterling@gmail.com>
2017-03-07 18:27:37 -0500

    Able to create new listings within angular form after auth integration

--- client/app/controllers/LandingCtrl.js
+++ client/app/controllers/LandingCtrl.js
@@ -2,22 +2,17 @@ app.controller("LandingCtrl", function($http, $location, $routeParams, UserFacto
   const landing = this
   landing.title = "Landing"
   landing.user = {}
-  
+
   UserFactory.getUser().then((res) => {
     landing.user = res
-    landing.requests()
+    landing.loadRequests()
     $timeout()
   })
-  // $scope.$on("user", function(event, data){
-  //   landing.user = data
-  //   console.log("LANDING DOT USER",landing.user)
-  //   $timeout()
-  // })
 
-  landing.requests= function(){
-    // $http.get(`http://localhost:8000/user/${landing.user.id}`) //${landing.userId}
-    //   .then(result => landing.user = result.data)
-    //     .then(() => console.log("from the get req in landingctrl",landing.user))
+  landing.loadRequests= function(){
+    $http.get(`http://localhost:8000/user/${landing.user.id}`) //${landing.userId}
+      .then(result => landing.user = result.data)
+        .then(() => console.log("from the get req in landingctrl",landing.user))
 
     $http.post("http://localhost:8000/get_messages",
     {"user": landing.user.id }, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
--- client/app/controllers/NewListingCtrl.js
+++ client/app/controllers/NewListingCtrl.js
@@ -1,26 +1,32 @@
-app.controller('NewListingCtrl', function($http, $location, authFactory) {
+app.controller('NewListingCtrl', function($http, $location, UserFactory, $timeout) {
   const newListing = this
   newListing.title = 'Post New Listing'
-  newListing.username = authFactory.user.username
-  newListing.userPk = authFactory.user.userId
+  newListing.user = {}
 
+  UserFactory.getUser().then((res) => {
+    newListing.user = res
+    $timeout()
+  })
+
+  // get request categories from API for dropdown menu choices
   $http.get("http://localhost:8000/get_request_categories")
     .then((response => newListing.request_categories = response.data))
       .then(() => console.log(newListing.request_categories))
 
+  // get grouping categories from API for dropdown menu choices
   $http.get("http://localhost:8000/get_grouping_choices")
     .then((response => newListing.grouping_choices = response.data))
       .then(() => console.log(newListing.grouping_choices))
 
   newListing.postNewListing = function() {
-    dataToPost = {"creator": `${newListing.userPk}`,
-    "category": newListing.chosenCategory[0],
-    "grouping": newListing.chosenGrouping[0],
-    "name": newListing.requestName,
-    "description": newListing.requestDescription,
-    "end": newListing.endDate,
-    "email": newListing.email,
-    "phone": newListing.phone}
+    dataToPost = {"creator": `${newListing.user.id}`,
+                  "category": newListing.chosenCategory[0],
+                  "grouping": newListing.chosenGrouping[0],
+                  "name": newListing.requestName,
+                  "description": newListing.requestDescription,
+                  "end": newListing.endDate,
+                  "email": newListing.email,
+                  "phone": newListing.phone}
 
     $http.post("http://localhost:8000/post_new_listing",
       dataToPost, {headers:{"Content-Type": 'application/x-www-form-urlencoded'}})
